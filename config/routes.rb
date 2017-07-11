4329affa2d233b0488177075b336fdf92dd023c3
Yoshi Tanaka <ytanaka.futbol1028@gmail.com>
2017-07-11 05:11:08 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
 
   root to: 'toppages#index'
   
-  get 'tasks/:id', to: 'tasks#show'
-  put 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
-  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
@@ -14,7 +10,6 @@ Rails.application.routes.draw do
   
   resources :users, only: [ :index, :show, :new, :create ]
 
-  resources :tasks, only: [:new, :edit, :create, :update, :destroy]
-  
-  
+  resources :tasks, only: [:new, :show, :edit, :create, :update, :destroy]
+
 end
